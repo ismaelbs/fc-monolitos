ddc7d15832c155acbcabb8c6763946a56d492779
Isma <ismabarboza@gmail.com>
2023-08-02 08:24:58 -0300

    feat: Adicionado facade para product-adm

---
+++ src/module/product-adm/facade/product-adm-facade.interface.ts
@@ -0,0 +1,21 @@
+export type AddProductFacadeInputDto = {
+    id: string;
+    name: string;
+    description: string;
+    purchasePrice: number;
+    stock: number;
+}
+
+// export type CheckStockInputDto = {
+//     productId: string;
+// }
+
+export type CheckStockOutputDto = {
+    productId: string;
+    stock: number;
+}
+
+export default interface ProductAdmFacadeInterface {
+    addProduct(input: AddProductFacadeInputDto): Promise<void>;
+    // checkProductStock(input: CheckStockInputDto): Promise<CheckStockOutputDto>;
+}
---
+++ src/module/product-adm/facade/product-adm-facade.ts
@@ -0,0 +1,26 @@
+import { UseCaseInterface } from "../../@shared/usecase/usecase-interface";
+import { AddProductOutputDto, AddProductInputDto } from "../usecases/dtos/add-product-dto";
+import ProductAdmFacadeInterface, { AddProductFacadeInputDto } from "./product-adm-facade.interface";
+
+type ProductAdmFacadeProps = {
+    // checkProductStockUseCase: UseCaseInterface;
+    addProductUseCase: UseCaseInterface<AddProductInputDto, AddProductOutputDto>;
+}
+
+export class ProductAdmFacade implements ProductAdmFacadeInterface {
+    // private _checkProductStockUseCase: UseCaseInterface;
+    private _addProductUseCase: UseCaseInterface<AddProductInputDto, AddProductOutputDto>;
+
+    constructor(input: ProductAdmFacadeProps) {
+        // this._checkProductStockUseCase = input.checkProductStockUseCase;
+        this._addProductUseCase = input.addProductUseCase;
+    }
+
+    async addProduct(input: AddProductFacadeInputDto): Promise<void> {
+        await this._addProductUseCase.execute(input);
+    }
+
+    // async checkProductStock(input: CheckStockInputDto): Promise<CheckStockOutputDto> {
+    //     return await this._checkProductStockUseCase.execute(input);
+    // }
+}
