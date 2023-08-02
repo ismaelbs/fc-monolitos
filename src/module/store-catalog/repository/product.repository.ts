0ba38a87f7bde6b8d4a13d51f5b2beb800efb923
Isma <ismabarboza@gmail.com>
2023-08-02 17:51:17 -0300

    feat: implementação do product repository store-catalog

--- src/module/store-catalog/repository/product.repository.ts
+++ src/module/store-catalog/repository/product.repository.ts
@@ -4,6 +4,20 @@ import { ProductGateway } from "@app/module/store-catalog/gateway/product.gatewa
 import { ProductModel } from "@app/module/store-catalog/repository/product.model";
 
 export class ProductRepository implements ProductGateway{
+    async find(id: Id): Promise<Product> {
+        const product = await ProductModel.findOne({ where: { id: id.value } });
+        if (!product) {
+            throw new Error('Product not found');
+        }
+        return new Product(
+            {
+                id: new Id(product.id),
+                name: product.name,
+                description: product.description,
+                salesPrice: product.salesPrice,
+            }
+        )
+    }
     async findAll(): Promise<Product[]> {
         const products = await ProductModel.findAll();
         return products.map(product => new Product(
