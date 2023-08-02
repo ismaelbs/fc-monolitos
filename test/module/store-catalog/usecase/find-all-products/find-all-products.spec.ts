b6f3814ebce1dabac1f49818a87fd89b1bc210e6
Isma <ismabarboza@gmail.com>
2023-08-02 17:35:00 -0300

    test: criado dto para usecase

--- test/module/store-catalog/usecase/find-all-products/find-all-products.spec.ts
+++ test/module/store-catalog/usecase/find-all-products/find-all-products.spec.ts
@@ -8,16 +8,16 @@ describe ('Product store catalog' , () => {
     it ('should find all products in store catalog', async () => {
         const usecase = new FindAllProductsUseCase(productRepositoryMock);
         const output = await usecase.execute();
-
         expect(productRepositoryMock.findAll).toBeCalled();
-        expect(output.length).toBe(2);
-        expect(output[0].id.value).toBe('1');
-        expect(output[0].name).toBe('product 1');
-        expect(output[0].description).toBe('description 1');
-        expect(output[0].salesPrice).toBe(10);
-        expect(output[1].id.value).toBe('2');
-        expect(output[1].name).toBe('product 2');
-        expect(output[1].description).toBe('description 2');
-        expect(output[1].salesPrice).toBe(20);
+        expect(output.products.length).toBe(2);
+
+        expect(output.products[0].id.value).toBe('1');
+        expect(output.products[0].name).toBe('product 1');
+        expect(output.products[0].description).toBe('description 1');
+        expect(output.products[0].salesPrice).toBe(10);
+        expect(output.products[1].id.value).toBe('2');
+        expect(output.products[1].name).toBe('product 2');
+        expect(output.products[1].description).toBe('description 2');
+        expect(output.products[1].salesPrice).toBe(20);
     })
 });
