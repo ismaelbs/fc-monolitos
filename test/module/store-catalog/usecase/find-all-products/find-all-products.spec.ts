65c333d7c856504195cc5458548c39ab698bf9f2
Isma <ismabarboza@gmail.com>
2023-08-02 17:42:58 -0300

    refactor: Usado desestruturação

--- test/module/store-catalog/usecase/find-all-products/find-all-products.spec.ts
+++ test/module/store-catalog/usecase/find-all-products/find-all-products.spec.ts
@@ -7,17 +7,17 @@ const productRepositoryMock = new ProductRepositoryMock();
 describe ('Product store catalog' , () => {
     it ('should find all products in store catalog', async () => {
         const usecase = new FindAllProductsUseCase(productRepositoryMock);
-        const output = await usecase.execute();
+        const { products } = await usecase.execute();
         expect(productRepositoryMock.findAll).toBeCalled();
-        expect(output.products.length).toBe(2);
+        expect(products.length).toBe(2);
 
-        expect(output.products[0].id.value).toBe('1');
-        expect(output.products[0].name).toBe('product 1');
-        expect(output.products[0].description).toBe('description 1');
-        expect(output.products[0].salesPrice).toBe(10);
-        expect(output.products[1].id.value).toBe('2');
-        expect(output.products[1].name).toBe('product 2');
-        expect(output.products[1].description).toBe('description 2');
-        expect(output.products[1].salesPrice).toBe(20);
+        expect(products[0].id.value).toBe('1');
+        expect(products[0].name).toBe('product 1');
+        expect(products[0].description).toBe('description 1');
+        expect(products[0].salesPrice).toBe(10);
+        expect(products[1].id.value).toBe('2');
+        expect(products[1].name).toBe('product 2');
+        expect(products[1].description).toBe('description 2');
+        expect(products[1].salesPrice).toBe(20);
     })
 });
