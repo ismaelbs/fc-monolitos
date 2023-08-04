c8d1facf31f111ce834ff81fadad34f20053a49c
Isma <ismabarboza@gmail.com>
2023-08-04 11:54:58 -0300

    test: implementação da facade de procurar cliente

--- test/module/client-adm/facade/client-adm.facade.spec.ts
+++ test/module/client-adm/facade/client-adm.facade.spec.ts
@@ -4,6 +4,7 @@ import { ClientModel } from "@app/module/client-adm/repository/client.model";
 import { AddClientUseCase } from "@app/module/client-adm/usecase/add-client/add-client.usecase";
 import { ClientAdmRepository } from "@app/module/client-adm/repository/client-adm.repository";
 import { ClientAdmFacade } from "@app/module/client-adm/facade/client-adm.facade";
+import { FindClientUseCase } from "@app/module/client-adm/usecase/find-client/find-client.usecase";
 
 let sequelize: Sequelize;
 
@@ -29,7 +30,9 @@ describe('ProductClientAdm', () => {
 
         const clientAdmRepository = new ClientAdmRepository();
         const usecase = new AddClientUseCase(clientAdmRepository);
-        const clientAdmFacade = new ClientAdmFacade(usecase);
+        const findUsecase = new FindClientUseCase(clientAdmRepository);
+
+        const clientAdmFacade = new ClientAdmFacade(usecase, findUsecase);
         const client = await clientAdmFacade.addClient(clientProps);
 
         expect(client).not.toBeNull();
@@ -37,6 +40,32 @@ describe('ProductClientAdm', () => {
         expect(client.name).toBe(clientProps.name);
         expect(client.email).toEqual(clientProps.email);
         expect(client.address).toEqual(clientProps.address);
+    })
+
+
+    it('should find a client via facade', async () => {
+        const clientAdmRepository = new ClientAdmRepository();
+        const addUsecase = new AddClientUseCase(clientAdmRepository);
+        const findUsecase = new FindClientUseCase(clientAdmRepository);
+        const clientAdmFacade = new ClientAdmFacade(addUsecase, findUsecase);
+
+        const clientProps = {
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+            address: 'John Doe address',
+        };
+
+        const output = await clientAdmFacade.addClient(clientProps);
+        
+        const client = await clientAdmFacade.find({
+            id: output.id
+        });
+
+        expect(client).not.toBeNull();
+        expect(client.id).toBe(output.id);
+        expect(client.name).toBe(clientProps.name);
+        expect(client.email).toBe(clientProps.email);
+        expect(client.address).toBe(clientProps.address);
     })
 
     afterEach(async () => {
