5317e17df4e0a12aa4ac0efe4892eb5c4b5ed901
Isma <ismabarboza@gmail.com>
2023-08-04 12:01:43 -0300

    test: introduzido clientadm factory

--- test/module/client-adm/facade/client-adm.facade.spec.ts
+++ test/module/client-adm/facade/client-adm.facade.spec.ts
@@ -1,10 +1,7 @@
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import { Sequelize } from "sequelize-typescript";
 import { ClientModel } from "@app/module/client-adm/repository/client.model";
-import { AddClientUseCase } from "@app/module/client-adm/usecase/add-client/add-client.usecase";
-import { ClientAdmRepository } from "@app/module/client-adm/repository/client-adm.repository";
-import { ClientAdmFacade } from "@app/module/client-adm/facade/client-adm.facade";
-import { FindClientUseCase } from "@app/module/client-adm/usecase/find-client/find-client.usecase";
+import { ClientAdmFacadeFactory } from "@app/module/client-adm/fatory/client-adm.facade.factory";
 
 let sequelize: Sequelize;
 
@@ -28,11 +25,7 @@ describe('ProductClientAdm', () => {
             address: 'John Doe address',
         };
 
-        const clientAdmRepository = new ClientAdmRepository();
-        const usecase = new AddClientUseCase(clientAdmRepository);
-        const findUsecase = new FindClientUseCase(clientAdmRepository);
-
-        const clientAdmFacade = new ClientAdmFacade(usecase, findUsecase);
+        const clientAdmFacade = ClientAdmFacadeFactory.create();
         const client = await clientAdmFacade.addClient(clientProps);
 
         expect(client).not.toBeNull();
@@ -44,10 +37,7 @@ describe('ProductClientAdm', () => {
 
 
     it('should find a client via facade', async () => {
-        const clientAdmRepository = new ClientAdmRepository();
-        const addUsecase = new AddClientUseCase(clientAdmRepository);
-        const findUsecase = new FindClientUseCase(clientAdmRepository);
-        const clientAdmFacade = new ClientAdmFacade(addUsecase, findUsecase);
+        const clientAdmFacade = ClientAdmFacadeFactory.create();
 
         const clientProps = {
             name: 'John Doe',
